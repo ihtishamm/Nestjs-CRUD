6295969d71d78a68eb75d6841a7931907e77abfc
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2025-02-12 01:31:24 -0800

    connect the render database and prepare for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,12 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT || '5432'),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     ItemModule,
     AuthModule,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
